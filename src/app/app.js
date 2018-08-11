06965ae1fc0a63d114a42967df9f5ab68920fc89
Benny <carlsson.benny93@gmail.com>
2018-08-11 21:33:50 +0200

    refacotring files

---
+++ src/app/app.js
@@ -0,0 +1,46 @@
+import React, { Component, Fragment } from "react"
+import StartPage from "./startPage"
+import RoomPage from "./roomPage"
+import firebase from "firebase"
+import { config } from "../superSecretFile"
+import "../App.css"
+import { getQueryParameter, addUrlParameter } from "./utils/urlParser"
+import { Page } from "./constants/page"
+
+class App extends Component {
+  constructor(props) {
+    super()
+    this.state = {
+      page: Page.StartPage
+    }
+  }
+
+  changePage = page => {
+    console.log("asd", page)
+    this.setState({ page: page })
+  }
+
+  componentDidMount() {
+    firebase.initializeApp(config)
+    const roomId = getQueryParameter("room")
+    if (roomId) {
+      this.setState({ page: Page.RoomPage })
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.page === Page.RoomPage ? (
+          <RoomPage />
+        ) : (
+          <Fragment>
+            <StartPage changePage={this.changePage} />
+          </Fragment>
+        )}
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/app/startPage/index.js
@@ -0,0 +1,34 @@
+import React from "react"
+import { Button } from "semantic-ui-react"
+import firebase from "firebase"
+import { addUrlParameter } from "../utils/urlParser"
+import { Page } from "../constants/page"
+export default class StartPage extends React.Component {
+  updateUrl = id => {
+    if (!window.history.pushState) {
+      alert("Your browser does not support this site")
+      return false
+    }
+    addUrlParameter("room", id)
+  }
+
+  createVote = id => {
+    this.props.changePage(Page.RoomPage)
+    this.updateUrl(123)
+    firebase
+      .database()
+      .ref("rooms/" + id)
+      .set({
+        question: "Vad ska vi äta idag?",
+        timeStamp: new Date().getTime()
+      })
+  }
+
+  render() {
+    return (
+      <Button size="massive" onClick={() => this.createVote(123)}>
+        Create Vote!
+      </Button>
+    )
+  }
+}
